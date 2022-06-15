6d763017ac0d2819dae52e7bdae9518aab68d25a
Dashrath kumar <dashrath.kumar@dotsquares.com>
2022-06-15 14:57:52 +0530

    setup kafka with docker & publisher in fask api and consumer

---
+++ consumer/app/__init__.py
@@ -0,0 +1,26 @@
+import logging
+from json import loads
+
+from app.enum import EnvironmentVariables as EnvVariables
+
+from kafka import KafkaConsumer
+
+
+def main():
+    try:
+        # To consume latest messages and auto-commit offsets
+        consumer = KafkaConsumer(
+            EnvVariables.KAFKA_TOPIC_NAME.get_env(),
+            bootstrap_servers=f'{EnvVariables.KAFKA_SERVER.get_env()}:{EnvVariables.KAFKA_PORT.get_env()}',
+            value_deserializer=lambda x: loads(x.decode('utf-8')),
+            auto_offset_reset='earliest',
+            enable_auto_commit=True,
+        )
+        print("Connection successful")
+        for message in consumer:
+            print("%s:%d:%d: key=%s value=%s" % (message.topic, message.partition,
+                                                 message.offset, message.key, message.value))
+
+    except Exception as e:
+        print(e)
+        logging.info('Connection error', e)
